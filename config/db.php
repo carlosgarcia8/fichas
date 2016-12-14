f6366425b15a98e6d60a4a5601ce62fe7aad193b
Juan Carlos Garcia <gjcarlos8@gmail.com>
2016-12-14 20:01:04 +0100

    Carga inicial

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=fichas',
+    'username' => 'fichas',
+    'password' => 'fichas',
+    'charset' => 'utf8',
+];
